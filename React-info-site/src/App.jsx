3ff61f5d92a668efb2cda664001029b7e9961a77
lizziegitau <lizziegitau0505@gmail.com>
2023-06-23 15:39:38 +0300

    first commit

---
+++ React-info-site/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
+import "./App.css"
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
